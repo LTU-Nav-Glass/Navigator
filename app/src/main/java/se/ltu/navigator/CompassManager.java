eb1fdfc900438acd750e1e2672f6f2e3e1a83b82
Alexandre Leconte <aleconte@dwightstudio.fr>
2025-01-29 12:36:30 +0100

    Adding compass logic using CompassManager

---
+++ app/src/main/java/se/ltu/navigator/CompassManager.java
@@ -0,0 +1,146 @@
+package se.ltu.navigator;
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.location.Location;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Compass logic manager responsible to change arrow and compass angle according to provided
+ * locations.
+ */
+public class CompassManager implements SensorEventListener {
+    public static final int SAMPLING_PERIOD_US = 10000;
+
+    private final MainActivity mainActivity;
+    private final SensorManager sensorManager;
+    private final Sensor rotationSensor;
+
+    // Data
+    private Location currentLocation;
+    private Location targetLocation;
+    private final float[] rotationMatrix = new float[16];
+    private final float[] orientationVector = new float[3];
+    private float currentAzimuth;
+    private float lastAzimuth;
+    private float currentBearing;
+    private float lastBearing;
+
+    public CompassManager(MainActivity mainActivity) {
+        this.mainActivity = mainActivity;
+        sensorManager = ((SensorManager) mainActivity.getSystemService(Context.SENSOR_SERVICE));
+        rotationSensor = sensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
+
+        // Initialize the rotation matrix to identity
+        rotationMatrix[ 0] = 1;
+        rotationMatrix[ 4] = 1;
+        rotationMatrix[ 8] = 1;
+        rotationMatrix[12] = 1;
+    }
+
+    /**
+     * Starts orientation monitoring.
+     */
+    public void startMonitoring() {
+        sensorManager.registerListener(this, rotationSensor, SAMPLING_PERIOD_US);
+    }
+
+    /**
+     * Stops orientation monitoring.
+     */
+    public void stopMonitoring() {
+        sensorManager.unregisterListener(this);
+    }
+
+    /**
+     * @param currentLocation The current location of the device.
+     */
+    public void setCurrentLocation(@NotNull Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+
+    /**
+     * @param targetLocation The current location of the target.
+     */
+    public void setTargetLocation(@NotNull Location targetLocation) {
+        this.targetLocation = targetLocation;
+    }
+
+    /**
+     * Called when there is a new sensor event.  Note that "on changed"
+     * is somewhat of a misnomer, as this will also be called if we have a
+     * new reading from a sensor with the exact same sensor values (but a
+     * newer timestamp).
+     *
+     * <p>See {@link SensorManager SensorManager}
+     * for details on possible sensor types.
+     * <p>See also {@link SensorEvent SensorEvent}.
+     *
+     * <p><b>NOTE:</b> The application doesn't own the
+     * {@link SensorEvent event}
+     * object passed as a parameter and therefore cannot hold on to it.
+     * The object may be part of an internal pool and may be reused by
+     * the framework.
+     *
+     * @param event the {@link SensorEvent SensorEvent}.
+     */
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_ROTATION_VECTOR) {
+            // We compute the rotation matrix from the rotation vector
+            SensorManager.getRotationMatrixFromVector(rotationMatrix, event.values);
+            // We compute the orientation vector from the rotation matrix
+            SensorManager.getOrientation(rotationMatrix, orientationVector);
+
+            // Azimuth is [0]
+            currentAzimuth = (float) Math.toDegrees(orientationVector[0]);
+
+            // Animate the rotation of the compass (disk & arrow)
+            RotateAnimation rotateCompass = new RotateAnimation(-lastAzimuth, -currentAzimuth, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+            rotateCompass.setDuration(SAMPLING_PERIOD_US/1000);
+            rotateCompass.setInterpolator(new LinearInterpolator());
+            rotateCompass.setFillAfter(true);
+
+            mainActivity.compass.startAnimation(rotateCompass);
+
+            lastAzimuth = currentAzimuth;
+
+            if (currentLocation != null && targetLocation != null) {
+                currentBearing = currentLocation.bearingTo(targetLocation);
+
+                // Animate the rotation of the compass (arrow)
+                RotateAnimation rotateArrow = new RotateAnimation(-lastBearing, -currentBearing, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                rotateArrow.setDuration(SAMPLING_PERIOD_US/1000);
+                rotateArrow.setInterpolator(new LinearInterpolator());
+                rotateArrow.setFillAfter(true);
+
+                mainActivity.compassArrow.startAnimation(rotateArrow);
+
+                lastBearing = currentBearing;
+            }
+        }
+    }
+
+    /**
+     * Called when the accuracy of the registered sensor has changed.  Unlike
+     * onSensorChanged(), this is only called when this accuracy value changes.
+     *
+     * <p>See the SENSOR_STATUS_* constants in
+     * {@link SensorManager SensorManager} for details.
+     *
+     * @param sensor
+     * @param accuracy The new accuracy of this sensor, one of
+     *                 {@code SensorManager.SENSOR_STATUS_*}
+     */
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Ignore?
+    }
+}
--- app/src/main/java/se/ltu/navigator/MainActivity.java
+++ app/src/main/java/se/ltu/navigator/MainActivity.java
@@ -1,31 +1,133 @@
 package se.ltu.navigator;
 
 import android.os.Bundle;
-import android.util.Log;
-
-import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
-import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.google.android.material.search.SearchBar;
+import com.google.android.material.search.SearchView;
 
 import se.ltu.navigator.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
+    /*
+     * View binding
+     */
+
     private static final String TAG = "MainActivity"; //Tag usually indicates class log message comes from
     private ActivityMainBinding binding;
 
+    // Search
+    protected SearchBar searchBar;
+    protected SearchView searchView;
+    protected RecyclerView searchResults;
+
+    // Compass
+    protected RelativeLayout compass;
+    protected ImageView compassDisk;
+    protected ImageView compassArrow;
+
+    // Navigation infos
+    protected LinearLayout bottomSheet;
+    protected BottomSheetBehavior<LinearLayout> bottomSheetBehavior;
+    protected RecyclerView navInfos;
+
+    /*
+     * Logic
+     */
+
+    // Compass logic
+    private CompassManager compassManager;
+
+    /**
+     * Method called when the view is created.
+     *
+     * @param savedInstanceState If the activity is being re-initialized after
+     *     previously being shut down then this Bundle contains the data it most
+     *     recently supplied in {@link #onSaveInstanceState}.  <b><i>Note: Otherwise it is null.</i></b>
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Create compass manager
+        compassManager = new CompassManager(this);
+        this.compassManager.startMonitoring();
+
+        // Binding
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        Log.d(TAG, "Hello World");
+        searchBar = findViewById(R.id.search_bar);
+        searchView = findViewById(R.id.search_view);
+        searchResults = findViewById(R.id.search_results);
+
+        compass = findViewById(R.id.compass);
+        compassDisk = findViewById(R.id.compass_disk);
+        compassArrow = findViewById(R.id.compass_arrow);
+
+        bottomSheet = findViewById(R.id.bottom_sheet);
+        bottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+
+        // Event handling
+
+        searchView.getEditText().setOnKeyListener(this::onSearchKeyTyped);
+        searchView.getEditText().setOnEditorActionListener(this::onSearchAction);
     }
 
+    /**
+     * Method called when the activity gains focus.
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        this.compassManager.startMonitoring();
+    }
+
+    /**
+     * Method called when the activity loses focus.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        this.compassManager.stopMonitoring();
+    }
+
+    /**
+     * Method called when a key is typed in the search bar.
+     *
+     * @param v The view of the event.
+     * @param keyCode The code of the key typed.
+     * @param event The corresponding event.
+     * @return True to consume the action, false otherwise.
+     */
+    private boolean onSearchKeyTyped(View v, int keyCode, KeyEvent event) {
+        // TODO: Update the recycler view `searchResults` with the room list
+        return false;
+    }
+
+    /**
+     * Method called when an action (other than typing) is performed on the search bar.
+     *
+     * @param v The view of the event.
+     * @param actionID The code of the action.
+     * @param event The corresponding event.
+     * @return True to consume the action, false otherwise.
+     */
+    private boolean onSearchAction(View v, int actionID, KeyEvent event) {
+        // TODO: Verify the name of the room before setting text
+        searchBar.setText(searchView.getText());
+        searchView.hide();
+        return false;
+    }
 }
